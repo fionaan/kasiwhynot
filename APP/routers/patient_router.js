ed2a876ffa21687615fbee06631de6e058b6c8cf
Andrew Lumayad <lumayad2121190@mkt.ceu.edu.ph>
2024-03-01 13:28:47 +0800

    nagtanggal lang ng comments sa router

--- APP/routers/patient_router.js
+++ APP/routers/patient_router.js
@@ -2,16 +2,7 @@ const express = require('express')
 const patientController = require('../controllers/patient_controller')
 const router = express.Router()
 
-//for reference lang
 router.post('/add', patientController.addRecord)
-
-
-
-
-// router.get('/filter', studentController.filterStudents)
-// router.delete('/delete/:id', studentController.deleteStudent)
-// router.put('/update/:id', studentController.updateStudent)
-
 router.get('/get/:pageNumber?', patientController.getPatientList)
 router.get('/getone', patientController.getPatient)
 router.get('/search/:pageNumber?', patientController.searchPatientList)
