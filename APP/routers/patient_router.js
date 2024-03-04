6ba9d53a230254816ba0270ae7fdc03e0dadbaa0
Angelo Cruz <78426303+AngeloCruz-ai@users.noreply.github.com>
2024-03-04 14:54:58 +0800

    patch

--- APP/routers/patient_router.js
+++ APP/routers/patient_router.js
@@ -6,6 +6,7 @@ router.post('/add', patientController.addRecord)
 router.get('/get/:pageNumber?', patientController.getPatientList)
 router.get('/getone', patientController.getPatient)
 router.get('/search/:pageNumber?', patientController.searchPatientList)
+router.put('/update/:id', patientController.updateRecord)
 
 module.exports = router
 
