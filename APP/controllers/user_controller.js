9ba080f5e2b0823e260cd306a816e8f5d87188e8
Rafael Pelagio <pelagio2120239@mkt.ceu.edu.ph>
2024-03-08 19:45:53 +0800

    view profile settings

--- APP/controllers/user_controller.js
+++ APP/controllers/user_controller.js
@@ -1,5 +1,51 @@
 const User = require('../models/user_model')
 
-module.exports = {
+const viewProfileSetting = async (req, res, next) => {
+    try {
+        const userId = req.body._id
 
+        // Validate if the userId is provided
+        if (!userId) {
+            return res.status(400).json({
+                successful: false,
+                message: 'User ID is required.'
+            })
+        }
+
+        const user = await User.findById(userId)
+
+        if (!user) {
+            return res.status(404).json({
+                successful: false,
+                message: 'User not found'
+            })
+        }
+
+        const passwordLength = user.password.length;
+        const maskedPassword = '*'.repeat(passwordLength)
+
+        const userProfile = {
+            name: user.name,
+            emailAddress: user.emailAddress,
+            userType: user.userType,
+            password: maskedPassword
+        }
+
+        res.json({
+            successful: true,
+            message: 'User profile retrieved successfully.',
+            userProfile: userProfile
+        })
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({
+            successful: false,
+            message: 'Internal Server Error'
+        })
+    }
+}
+
+
+module.exports = {
+    viewProfileSetting
 }
--- APP/routers/user_router.js
+++ APP/routers/user_router.js
@@ -1,13 +1,13 @@
 const express = require('express')
 const studentController = require('../controllers/user_controller')
 const router = express.Router()
+const userController = require('../controllers/user_controller')
 
 //for reference lang
 // router.post('/add', studentController.addStudent)
-// router.get('/getAll', studentController.getAllStudents)
+router.get('/viewProfile', userController.viewProfileSetting)
 // router.get('/filter', studentController.filterStudents)
 // router.delete('/delete/:id', studentController.deleteStudent)
 // router.put('/update/:id', studentController.updateStudent)
 
 module.exports = router
-
