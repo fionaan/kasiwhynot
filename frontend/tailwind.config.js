3ed08371660dc9b0ce4d1e4ee9e0752d7f411590
Andrew Lumayad <lumayad2121190@mkt.ceu.edu.ph>
2024-04-20 07:29:26 +0800

    preset colors

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,6 +3,13 @@ module.exports = {
   content: ["./views/**/*.{html,js}"],
   theme: {
     extend: {},
+    colors: {
+      "bgColor": "E2E2E2",
+      "overlay": "001F54",
+      "overButton": "ABC8FF",
+      "darkBlutton": "001F44",
+      "popupColor": "BFD9FF"
+    },
   },
   plugins: [],
 }
