71667b04896a41f00fd8f020ee45b1711700b648
Andrew Lumayad <lumayad2121190@mkt.ceu.edu.ph>
2024-04-19 20:20:56 +0800

    transitioned to OTP system (setNewPassword, changeOldPassword, new OTP functions in email controller), accessTokens now stored in httponlyCookie

--- backend/APP/controllers/email_controller.js
+++ backend/APP/controllers/email_controller.js
@@ -1,6 +1,10 @@
 require('dotenv').config()
 const User = require('../models/user_model')
-const nodemailer = require('nodemailer');
+const nodemailer = require('nodemailer')
+const randomString = require('randomstring')
+const utilFunc = require('../../utils')
+
+const OTPcache = {};
 
 const transporter = nodemailer.createTransport({
     service: 'gmail',
@@ -50,6 +54,83 @@ const sendCode = async (req, res, next) => {
     }
 }
 
+const reqOTP = async (req, res, next) => {
+    const {email} = req.body
+    const otp = generateOTP()
+    OTPcache[email] = otp //store OTP in cache
+
+    let user = await User.findOne({ emailAddress: email })
+
+    if (utilFunc.checkIfNull(email)) {
+        return res.status(400).send("Email is not specified.")
+    }
+    if (utilFunc.checkIfNull(OTPcache[email])) {
+        return res.status(400).send("OTP value failed to be stored.")
+    }
+    if (user == null) {
+        return res.status(400).send("The specified user does not exist in the database.")
+    }
+
+    sendOTP(email, otp)
+    res.cookie('otpCache', OTPcache, {maxAge: 30000, httpOnly: true})
+    console.log(OTPcache)
+    console.log(otp)
+    res.status(200).json({message: "OTP sent successfully."})
+}
+
+const verifyOTP = async (req, res, next) => {
+    const {email, otp} = req.body
+
+    if (!OTPcache.hasOwnProperty(email)) {
+        return res.status(400).json({message: "Email not found."})
+    }
+
+    const stringOTP = String(OTPcache[email]) //ensures that OTP will always be converted into a string
+
+    if (stringOTP === otp.trim()) {
+        delete OTPcache[email]
+        return res.status(200).json({message: "OTP verified successfully"})
+        //then redirect to setNewPassword page
+    }
+    else {
+        return res.status(400).json({message: "Invalid OTP."})
+    }
+}
+
+function generateOTP() {
+    return randomString.generate({length: 4, charset: 'numeric'})
+}
+
+function sendOTP(email, otp) {
+    try {
+        const mailOptions = {
+            from: {
+                name: 'Healthy CEU',
+                address: process.env.SYSTEM_EMAIL
+            },
+            to: email,
+            subject: 'OTP request.',
+            text: `Your OTP is: ${otp}`
+        }
+
+        transporter.sendMail(mailOptions, (error, info) => {
+            if (error) {
+                console.error(error)
+                return res.status(500).send('Failed to send email')
+            } else {
+                console.log('Email sent: ' + info.response)
+                return res.status(200).send('Email sent successfully')
+            }
+        })
+    }
+    catch (error) {
+        console.error(error)
+        res.status(500).send('Internal server error')
+    }
+}
+
 module.exports = {
-    sendCode
+    sendCode,
+    reqOTP,
+    verifyOTP
 }
--- backend/APP/routers/email_router.js
+++ backend/APP/routers/email_router.js
@@ -3,5 +3,7 @@ const emailController = require('../controllers/email_controller')
 const router = express.Router()
 
 router.get('/sendcode', emailController.sendCode)
+router.post('/req-otp', emailController.reqOTP)
+router.post('/verify-otp', emailController.verifyOTP)
 
 module.exports = router
