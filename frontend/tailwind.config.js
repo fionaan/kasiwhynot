ff4b30237ab999a57c854f92d3ea3ccc714fd359
DESKTOP-MKLROQ5\User <ceu>
2024-04-20 10:28:32 +0800

    4/20 Homepages edited

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./views/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "bgcolor": "E2E2F2",
+        "overlay": "001F54"
+      }
+    },
   },
   plugins: [],
 }
