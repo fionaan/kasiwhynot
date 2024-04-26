6f677a24fc6f37d3c040172f7f82d55747dd1b60
Angelo D. Cruz~ <cruz1920755@mkt.ceu.edu.ph>
2024-04-26 23:51:46 +0800

    04/26 adjusted homepage, added category redirection

--- frontend/Homepages/js/DentalHome.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    var profileBtn = document.getElementById("profileBtn");
-    var dropdownMenu = document.getElementById("dropdownMenu");
-
-    profileBtn.addEventListener("click", function () {
-        dropdownMenu.classList.toggle("hidden");
-    });
-});
