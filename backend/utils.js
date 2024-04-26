ead8547ef8aca1363ca193132acfb114711e66fb
Fiona Mariano <mariano2120398@mkt.ceu.edu.ph>
2024-04-26 19:32:43 +0800

    docs: modified comments

--- backend/utils.js
+++ backend/utils.js
@@ -11,7 +11,7 @@ const dateTimeRegex = /^(?:\d{4})-(?:0[1-9]|1[0-2])-(?:0[1-9]|[1-2][0-9]|3[0-1])
 //CHECKS IF DATA CONTAINS DATE ONLY
 const dateRegex = /^(?:19|20)\d{2}-(?:0[1-9]|1[0-2])-(?:0[1-9]|[12][0-9]|3[01])T(?:[01]\d|2[0-3]):[0-5]\d:[0-5]\d\.\d{3}Z$/
 
-//CHECKS IF STRING ONLY CONTAINS LETTERS, PERIOD, APOSTROPHE, HYPHEN, OR SPACE - MUST START W LETTER
+//CHECKS IF STRING ONLY CONTAINS LETTERS, APOSTROPHE, HYPHEN, OR SPACE - MUST START AND END W LETTER
 const nameRegex = /^[a-zA-Z]+(?:[\s-]*[a-zA-Z]+)*$/ // /^[a-zA-Z][a-zA-Z.,'\s-]*$/
 
 //CHECKS IF EMAIL IS VALID
