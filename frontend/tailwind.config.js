cb313da212b35c45a7bc267d26881a19b27c8e77
Fiona Mariano <mariano2120398@mkt.ceu.edu.ph>
2024-05-10 03:11:35 +0800

    feat: improved viewProfile.html, ongoing forgotPassword.html

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,10 +4,13 @@ module.exports = {
   theme: {
     extend: { 
       colors: {
-      "bgColor": "#E2E2F2",
+      "bgColor": "EEEEFF",
+      "bgColorWh": "FDFDFD",
       "overlay": "001F54",
       "overButton": "ABC8FF",
       "darkButton": "001F44",
+      "hoverDarkBlue": "004496",
+      "activeDarkBlue": "00144F",
       "popupColor": "BFD9FF"
       },
 
@@ -16,7 +19,9 @@ module.exports = {
       },
 
       height: {
-        'login': '22rem'
+        'login': '22rem',
+        'computed-height': 'calc(96vh - (48px + 88px + 32px))',
+        'medium-pic': '9px'
       }
     },
 
